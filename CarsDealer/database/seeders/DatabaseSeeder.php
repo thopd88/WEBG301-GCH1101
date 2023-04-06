05574599796cef23a96d06304e8c69cc9fd4e871
Pham Duc Tho <20041501+thopd88@users.noreply.github.com>
2023-04-06 15:14:08 +0700

    Update DatabaseSeeder.php

--- CarsDealer/database/seeders/DatabaseSeeder.php
+++ CarsDealer/database/seeders/DatabaseSeeder.php
@@ -3,6 +3,10 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\Cars;
+use App\Models\Customers;
+use App\Models\Sales;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -12,11 +16,54 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+        $car1 = new Cars(
+            [
+                'Make' => 'Audi',
+                'Model' => 'A4',
+                'TravelledDistance' => 100000,
+            ]
+        );
+        $car2 = new Cars(
+            [
+                'Make' => 'Toyota',
+                'Model' => 'Camry',
+                'TravelledDistance' => 200000,
+            ]
+        );
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        $car1->save();
+        $car2->save();
+
+        $customer1 = new Customers(
+            [
+                'Name' => 'John',
+                'BirthDate' => '1990-01-01',
+                'IsYoungDriver' => true,
+            ]
+        );
+
+        $customer1->save();
+
+        $customer2 = new Customers(
+            [
+                'Name' => 'Jane',
+                'BirthDate' => '1995-01-01',
+                'IsYoungDriver' => false,
+            ]
+        );
+
+        $customer2->save();
+
+        $sale1 = new Sales(
+            [
+                'Discount' => 0.1,
+                'CarId' => $car1->Id,
+                'CustomerId' => $customer1->Id,
+            ]
+        );
+
+        $sale1->save();
+
+
     }
 }
